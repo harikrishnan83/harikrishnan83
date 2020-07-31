926d7c3305527c33160239cc73596d4efa8ff813
Hari Krishnan <harikrishnan83@gmail.com>
2020-07-31 23:16:02 +0530

    test readme generation

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ md.use(emoji);
     console.error(`Failed to load blog posts from ${websiteUrl}`, e);
   }
 
-  text = `![Banner](/harikrishnan83/harikrishnan83/blob/master/profile_banner.png)\n\n[![Linkedin: harikrishnan83](https://img.shields.io/badge/-harikrishnan83-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/harikrishnan83/)](https://www.linkedin.com/in/harikrishnan83/) [![GitHub: harikrishnan83](https://img.shields.io/github/followers/harikrishnan83?label=follow&style=social)](https://github.com/harikrishnan83) [![Twitter: harikrishnan83](https://img.shields.io/twitter/follow/harikrishnan83?style=social)](https://twitter.com/harikrishnan83)\n\n## Medium posts\n\n${blogPosts}`;
+  text = `![Banner](/harikrishnan83/harikrishnan83/blob/master/profile_banner.png)\n\n[![Linkedin: harikrishnan83](https://img.shields.io/badge/-harikrishnan83-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/harikrishnan83/)](https://www.linkedin.com/in/harikrishnan83/) [![GitHub: harikrishnan83](https://img.shields.io/github/followers/harikrishnan83?label=follow&style=social)](https://github.com/harikrishnan83) [![Twitter: harikrishnan83](https://img.shields.io/twitter/follow/harikrishnan83?style=social)](https://twitter.com/harikrishnan83)![Build README](https://github.com/harikrishnan83/harikrishnan83/workflows/Build%20README/badge.svg)\n\n## Medium posts\n\n${blogPosts}`;
 
   const result = md.render(text);
 
